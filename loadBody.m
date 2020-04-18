3a6de2cd09a0ac8184c8604b28bd26d5e3e88fc4
AchilleH <achillehebertiii@gmail.com>
2020-04-18 12:27:12 -0700

    did not commit typo fix

--- loadBody.m
+++ loadBody.m
@@ -11,20 +11,20 @@ n = 4; %number for end conditions (both fixed = 4)
 
 %% Stress Calc and Wall thickness solver
 tWall = 0.035; %starting wall thickness [in]
-aC = pi() * (rocket.geo.body.D^2 - (rocket.geo.body.D-tWall)^2);
+aC = pi * (rocket.geo.body.D^2 - (rocket.geo.body.D-tWall)^2);
 stress = rocket.prop.T_avg/aC;
 
 minFOS = Yield/stress;
 while (minFOS < 2 || minFOS > 2.3)
     tWall = tWall + 0.005;
-    aC = pi() * (rocket.geo.body.D^2 - (rocket.geo.body.D-tWall)^2);
+    aC = pi * (rocket.geo.body.D^2 - (rocket.geo.body.D-tWall)^2);
     stress = rocket.prop.T_avg/aC;
 
     minFOS = Yield/stress;
 end
 
 %% Buckling length
-I = pi()/64 * (rocket.geo.body.D^4 - (rocket.geo.body.D-tWall)^4);
+I = pi/64 * (rocket.geo.body.D^4 - (rocket.geo.body.D-tWall)^4);
 
 maxL = sqrt((n*pi^2*E*I)/Yield);
 
