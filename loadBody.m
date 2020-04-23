35af8cd38089cedfea6ea015e1120e7ef826684a
AchilleH <achillehebertiii@gmail.com>
2020-04-22 17:55:52 -0700

    Still need to debug but theoretically done. material library implemented and load body accounts for the weight of a unit long body tube section.

--- loadBody.m
+++ loadBody.m
@@ -6,23 +6,35 @@ function [maxL,minFOS,tWall] = loadBody(rocket)
 %note: freedom units
 Yield = rocket.geo.body.material.yield;
 E = rocket.geo.body.material.young;
+ro = rocket.geo.body.D/2;
+T = rocket.prop.T_avg;
+rho = rocket.geo.body.material.density;
 
 n = 4; %number for end conditions (both fixed = 4)
 
 %% Stress Calc and Wall thickness solver
 tWall = 0.035; %starting wall thickness [in]
-aC = pi * (rocket.geo.body.D^2 - (rocket.geo.body.D-tWall)^2);
-stress = rocket.prop.T_avg/aC;
-
+aC = pi * (ro^2 - (ro-tWall)^2);
+Tstress = rocket.prop.T_avg/aC; %longitudinal stress from thrust
+Wstress = rho*g; % this assumes a unit long section of body tube
+stress = Tstress + Wstress;
 minFOS = Yield/stress;
-while (minFOS < 2 || minFOS > 2.3)
+i = 1; %count variable to avoid infinite
+while (minFOS < 2 || minFOS > 2.3 && i < 10000)
     tWall = tWall + 0.005;
-    aC = pi * (rocket.geo.body.D^2 - (rocket.geo.body.D-tWall)^2);
-    stress = rocket.prop.T_avg/aC;
-
+    aC = pi * (ro^2 - (ro-tWall)^2);
+    Tstress = T/aC;
+    Wstress = rho*g; % this assumes a unit long section of body tube
+    stress = Tstress + Wstress;
     minFOS = Yield/stress;
 end
 
+if(i = 10000)
+    maxL = 0;
+    minFOS = 0;
+    tWall = 0;
+    return;
+end
 %% Buckling length
 I = pi/64 * (rocket.geo.body.D^4 - (rocket.geo.body.D-tWall)^4);
 
--- mLibrary.m
+++ mLibrary.m
@@ -3,15 +3,26 @@ function mLibrary = mLibrary()
     %to go from MPa to psi, multiply by 145.038, 0.062428 to go to lbm/ft^3
 
     % Aluminum
-    mLibrary.Al.one = material('2014',96.5*145.038,73*145.038,2800*0.062428);
-    mLibrary.Al.two = material('2018',317.1*145.038,74*145.038,2800*0.062428);
-    mLibrary.Al.three = material('2024',75.8*145.038,73*145.038,2800*0.062428);
-    mLibrary.Al.four = material('2618-T61(SS)',371.9*145.038,74.4999*145.038,2760*0.062428);
-    mLibrary.Al.five = material('6061-T6(SS)',275*145.038,69*145.038,2700*0.062428);
-    mLibrary.Al.six = material('6063-T83',240*145.038,69*145.038,2700*0.062428);
-    mLibrary.Al.seven = material('7050-T7651',490*145.038,72*145.038,2830*0.062428);
-    mLibrary.Al.eight = material('7075-T6(SN) (usual choice)',505*145.038,72*145.038,2810*0.062428);
+    mLibrary.Al.a = material('2014',96.5*145.038,73*145.038,2800*0.062428);
+    mLibrary.Al.b = material('2018',317.1*145.038,74*145.038,2800*0.062428);
+    mLibrary.Al.c = material('2024',75.8*145.038,73*145.038,2800*0.062428);
+    mLibrary.Al.d = material('2618-T61(SS)',371.9*145.038,74.4999*145.038,2760*0.062428);
+    mLibrary.Al.e = material('6061-T6(SS)',275*145.038,69*145.038,2700*0.062428);
+    mLibrary.Al.f = material('6063-T83',240*145.038,69*145.038,2700*0.062428);
+    mLibrary.Al.g = material('7050-T7651',490*145.038,72*145.038,2830*0.062428);
+    mLibrary.Al.h = material('7075-T6(SN) (usual choice)',505*145.038,72*145.038,2810*0.062428);
 
     % Steel
-    mLibrary.St.one = material('7075-T6(SN) (usual choice)',505*145.038,72*145.038,2810*0.062428);
+    mLibrary.St.a = material('Alloy Steel',620*145.038,210*145.038,7700*0.062428);
+    mLibrary.St.b = material('AISI 4340 normalized',710*145.038,205*145.038,7850*0.062428);
+
+    % Titanium
+    mLibrary.Ti.a = material('Ti-8Al-1Mo-1V annealed',930.79*145.038,120*145.038,4369.9*0.062428);
+
+    % Fiberglass
+    mLibrary.Ti.a = material('Amalga Fiberglass',27000,106*1.3,0.072*12^3);
+
+    % Carbon fiber
+    % the yield and youngs modulus are in ksi but converted to psi
+    mLibrary.C.a = material('Carbon Fiber 45% resin',515.54*1000*145.038,18898*1000,1600*0.062428);
 end
